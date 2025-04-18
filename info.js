80e8d8125feaedbabaafeb401f336741d1dda78a
jovanni villa <49220521+JovanniVilla@users.noreply.github.com>
2025-04-18 02:09:52 -0600

    Add files via upload

---
+++ info.js
@@ -0,0 +1,31 @@
+export const info = {
+  legalName: "Giove",
+  companyName: "Giove",
+  description: "Joyas exclusivas de plata y oro, con piedras preciosas",
+  email: {
+    sender: "",
+    recipients: [""],
+    subject: "Nuevo prospecto Giove",
+  },
+  phoneNumber: "+523335596433",
+  whatsapp: {
+    value: "+523335596433",
+    message: "Buen día, quiero cotizar el diseño de una pieza",
+  },
+  social: {
+    facebook: "",
+    instagram: "",
+  },
+  address: {
+    address: "",
+    col: "",
+    cp: "",
+    city: "",
+    state: "",
+  },
+  optInWebhook: 'https://hook.us1.make.com/6x2weapk31u2p1vvcaqm5sdnjmjldxnd',
+  surveyWebhook: 'https://hook.us1.make.com/jvjg4avfhuchyaeyma73ffo7m1285cwq',
+  surveyRedirect: '',
+  privacyNotice: "/aviso-privacidad",
+  termsConditions: "/aviso-privacidad",
+};
